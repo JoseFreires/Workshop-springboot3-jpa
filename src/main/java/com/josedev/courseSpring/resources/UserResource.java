e6da12fd0533eddbf2f1584ec51a284d0e565494
Jose Freires <joseguifreires@gmail.com>
2025-06-25 17:00:32 -0300

    Created UserService and Updated UserResource (Service layer, component registration)

--- src/main/java/com/josedev/courseSpring/resources/UserResource.java
+++ src/main/java/com/josedev/courseSpring/resources/UserResource.java
@@ -1,18 +1,37 @@
 package com.josedev.courseSpring.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.josedev.courseSpring.entities.User;
+import com.josedev.courseSpring.repositories.UserRepository;
+import com.josedev.courseSpring.service.UserService;
+
 
 @RestController // Definindo que essa classe vai ser uma controladora REST
 @RequestMapping(value = "/users") // Definindo que essa classe vai ter esse endereço de web
 public class UserResource {
+	
+	@Autowired
+	private UserService service;
+
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Xande", "xandinhoS2@gmail.com", "999999999", "123456");
-		return ResponseEntity.ok().body(u); // Definindo que se estiver OK aprensentar o "body"
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }

75051b3916ad96237d3248164f183760ad8b3a3e
Jose Freires <joseguifreires@gmail.com>
2025-07-03 11:58:32 -0300

    Created Payment (one-to-one association)

--- src/main/java/com/josedev/courseSpring/config/TestConfig.java
+++ src/main/java/com/josedev/courseSpring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.josedev.courseSpring.entities.Category;
 import com.josedev.courseSpring.entities.Order;
 import com.josedev.courseSpring.entities.OrderItem;
+import com.josedev.courseSpring.entities.Payment;
 import com.josedev.courseSpring.entities.Product;
 import com.josedev.courseSpring.entities.User;
 import com.josedev.courseSpring.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(p5, o1, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 	

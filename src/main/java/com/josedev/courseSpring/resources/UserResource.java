21bdc27c5b99ce8997c42099bb5cbf00975a5611
Jose Freires <joseguifreires@gmail.com>
2025-07-03 16:53:46 -0300

    User update

--- src/main/java/com/josedev/courseSpring/resources/UserResource.java
+++ src/main/java/com/josedev/courseSpring/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,10 +49,18 @@ public class UserResource {
 		return ResponseEntity.created(uri).body(obj);
 	}
 	
+	// Realiza a exclusão do dado no banco
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> delete(@PathVariable Long id, @RequestBody User obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 	
 }

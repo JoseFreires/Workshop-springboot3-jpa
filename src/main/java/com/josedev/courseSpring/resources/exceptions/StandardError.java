9a70af2b0a31f11baae44c260f6545033cb516a1
Jose Freires <joseguifreires@gmail.com>
2025-07-04 16:58:35 -0300

    Exception handling - delete

--- src/main/java/com/josedev/courseSpring/resources/exceptions/StandardError.java
+++ src/main/java/com/josedev/courseSpring/resources/exceptions/StandardError.java
@@ -3,10 +3,13 @@ package com.josedev.courseSpring.resources.exceptions;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class StandardError implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-mm-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant timestamp;
 	private Integer status;
 	private String error;
